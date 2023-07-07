9ca3a2b89ca954a91f7a12e8fae9bdce43c46e61
chakri-chax <lovachakravarthi@gmail.com>
2023-07-07 16:19:45 +0530

    commit

---
+++ src/GreetMsg.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import abi from './greeterABI.json'
+
+import React from 'react'
+
+function GreetMsg() {
+    const[greet,setGreet] = useState("")
+    const[branch,setBranch] = useState('')
+    const contractAddress = '0x2d86EC1a89e2411C495880651E5e93e91666D5b9'
+    const contractABI = abi;
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const contract = new ethers.Contract(contractAddress,contractABI,provider)
+
+    
+
+const getGreet = async(e)=>{
+    const greetMsg = await contract.greet();
+    const branch = await contract.getBranch();
+    setGreet(greetMsg)
+    setBranch(branch)
+}
+  return (
+    <div>
+
+        <button onClick={getGreet}>
+                Get Greet
+        </button>
+
+        <p>{greet}</p>
+        <p>{branch}</p>
+    </div>
+  )
+}
+
+export default GreetMsg
+
+
+
+
+
