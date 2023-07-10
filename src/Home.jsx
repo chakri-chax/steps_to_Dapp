b42a7208504118e1cafe6e591ef6a641b7675aaf
chakri-chax <lovachakravarthi@gmail.com>
2023-07-10 17:51:42 +0530

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,28 @@
 import React from "react";
+import ui from './images/Library ui.png'
 
 import { Outlet, Link } from "react-router-dom";
+import Ui from "./Ui";
 const Home = () => {
   return (
-    <div className="home-page">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li><br/>
-          <li>
-            <Link to="/Form">Borrow a Book</Link>
-          </li><br/>
-          <li>
-            <Link to="/MyBooks">Mybooks</Link>
-          </li><br/>
-        </ul>
-      </nav>
-    </div>
+    // <div className="bgImage">
+    //   <img src={ui}  alt="No Image"/>
+    //   {/* <nav>
+    //     <ul>
+    //       <li>
+    //         <Link to="/">Home</Link>
+    //       </li><br/>
+    //       <li>
+    //         <Link to="/Form">Borrow a Book</Link>
+    //       </li><br/>
+    //       <li>
+    //         <Link to="/MyBooks">Mybooks</Link>
+    //       </li><br/>
+    //     </ul>
+    //   </nav> */}
+    // </div>
+
+    <Ui/>
   );
 };
 
---
+++ src/Ui.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import girl from './images/girl.png'
+const Ui = () => {
+  return (
+    <div className="Desktop1 w-96 h-96 relative bg-white">
+  {/* <div className="Navybox w-96 h-96 left-[854px] top-0 absolute bg-blue-950" /> */}
+  <div className="w-3/5 bg-red"></div>
+      <div className="w-2/5 bg-black"></div>
+  <div className="Circles w-96 h-96 left-[857px] top-[393px] absolute">
+    <div className="Middlecircle w-64 h-64 left-[77px] top-[75px] absolute bg-slate-900 bg-opacity-60 rounded-full" />
+    <div className="Outercircle w-96 h-96 left-0 top-0 absolute bg-blue-950 bg-opacity-0 rounded-full border border-stone-300" />
+    <div className="Innercircle w-48 h-48 left-[119px] top-[116px] absolute bg-stone-300 rounded-full" />
+    <div className="Bulbcirc w-16 h-16 left-[304px] top-[12px] absolute bg-stone-300 rounded-full" />
+    <div className="Codecirc w-16 h-16 left-[389px] top-[166px] absolute bg-stone-300 rounded-full" />
+    <div className="Growthcir w-16 h-16 left-[304px] top-[318px] absolute bg-stone-300 rounded-full" />
+    <img className="Code1 w-12 h-10 left-[399px] top-[182px] absolute" src="https://via.placeholder.com/48x40" />
+    <img className="IdeaBulb1 w-12 h-12 left-[313px] top-[23px] absolute" src="https://via.placeholder.com/48x52" />
+    <img className="Growth1 w-12 h-12 left-[313px] top-[327px] absolute" src="https://via.placeholder.com/48x48" />
+  </div>
+  <div className="Girl w-96 h-96 left-[640px] top-[249px] absolute">
+    <div className="Girlbox w-96 h-96 left-0 top-0 absolute bg-white" />
+    <img className="Girl1 w-96 h-96 left-0 top-0 absolute" src={girl} />
+  </div>
+  <div className="Group15 w-96 h-12 left-[960px] top-[31px] absolute">
+    <div className="Connect w-40 h-12 left-[201px] top-0 absolute">
+      <div className="Rectangle3 w-40 h-12 left-0 top-0 absolute bg-slate-900 bg-opacity-95 rounded--3xl" />
+      <div className="Connect w-36 h-9 left-[11px] top-[8px] absolute text-center text-white text-xl font-bold">CONNECT</div>
+    </div>
+    <div className="Signup w-40 h-12 left-0 top-0 absolute">
+      <div className="Rectangle3 w-40 h-12 left-0 top-0 absolute bg-slate-900 bg-opacity-95 rounded--3xl" />
+      <div className="Signup w-36 h-9 left-[11px] top-[8px] absolute text-center text-white text-xl font-bold">SignUp</div>
+    </div>
+  </div>
+  <div className="Header w-96 h-10 left-[54px] top-[37px] absolute">
+    <img className="Pngwing1 w-10 h-10 left-0 top-0 absolute" src="https://via.placeholder.com/40x39" />
+    <div className="Home w-20 h-9 left-[266px] top-[3px] absolute text-black text-2xl font-bold">Home</div>
+    <div className="Borrow w-24 h-9 left-[384px] top-[3px] absolute text-black text-2xl font-bold">Borrow</div>
+    <div className="Store w-20 h-9 left-[523px] top-[3px] absolute text-black text-2xl font-bold">Store</div>
+    <div className="Contact w-24 h-9 left-[666px] top-[3px] absolute text-black text-2xl font-bold">Contact</div>
+    <div className="LibraryClaw w-40 h-9 left-[51px] top-[3px] absolute text-black text-2xl font-bold">Library Claw</div>
+  </div>
+  <div className="Quotes w-96 h-80 left-[54px] top-[318px] absolute">
+    <div className="ExploreYourFavoriteBooks w-80 h-8 left-0 top-[12.64px] absolute text-black text-xl font-normal">Explore Your Favorite Books </div>
+    <img className="PngtreeBooksLogo4135439RemovebgPreview1 w-16 h-14 left-[269px] top-0 absolute" src="https://via.placeholder.com/68x57" />
+    <div className="ALibraryIsAHospitalForTheMind w-96 h-56 left-0 top-[90px] absolute text-black text-6xl font-normal">"A LIBRARY IS A HOSPITAL FOR THE MIND."</div>
+  </div>
+  <div className="Footerbooks w-96 left-[94px] top-[828px] absolute">
+    <div className="Group12 w-80 h-32 left-0 top-0 absolute">
+      <div className="Book2 w-24 h-32 left-[238px] top-0 absolute">
+        <div className="Rectangle4 w-24 h-32 left-0 top-0 absolute bg-stone-300 rounded--xl" />
+        <img className="Book21 w-20 h-28 left-[9.30px] top-[8.46px] absolute" src="https://via.placeholder.com/75x109" />
+      </div>
+      <div className="Book5 w-24 h-32 left-[119px] top-0 absolute">
+        <div className="Rectangle4 w-24 h-32 left-0 top-0 absolute bg-stone-400 rounded--xl" />
+        <img className="Book51 w-20 h-28 left-[9px] top-[5px] absolute" src="https://via.placeholder.com/73x112" />
+      </div>
+      <div className="Book3 w-24 h-32 left-0 top-0 absolute">
+        <img className="Book31 w-20 h-28 left-[7.44px] top-[8.46px] absolute" src="https://via.placeholder.com/79x108" />
+        <div className="Rectangle4 w-24 h-32 left-0 top-0 absolute bg-slate-700 bg-opacity-30 rounded--xl" />
+      </div>
+    </div>
+    <div className="Arrow w-16 h-14 left-[351px] top-[33px] absolute">
+      <div className="Arrowcircle w-14 h-14 left-0 top-0 absolute bg-stone-300 rounded-full border border-black" />
+      <img className="ArrowRemovebgPreview1 w-14 h-12 left-[3px] top-[4px] absolute" src="https://via.placeholder.com/60x51" />
+    </div>
+  </div>
+</div>
+    
+
+        
+  
+    
+  )
+}
+
+export default Ui
